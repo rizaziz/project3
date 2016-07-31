00fa395f111aeff2858c4e0c6f66561452f79992
riz <riz_aziz2002@mail.ru>
2016-07-31 09:48:01 -0400

    initial commit

---
+++ src/somepackage/SomeClass.java
@@ -0,0 +1,14 @@
+package somepackage;
+
+public class SomeClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from Eclipse!");
+		System.out.println("Hello from Eclipse!");
+		System.out.println("hello from Eclipse!");
+		System.out.println("djfsdkfksf");
+
+	}
+
+}
